497ba368ff07567e76802cb8d90cc7a4211cf71d
Antonio SJ Musumeci <trapexit@spawn.link>
2025-01-21 21:55:22 -0600

    Utilities.cpp

--- src/3DOHomebrewSigning/Utilities.cpp
+++ src/3DOHomebrewSigning/Utilities.cpp
@@ -14,7 +14,7 @@ void CalculateMD5(char *_pBuffer, int _iDataSize, char *_pMD5)
   MD5Final(_pMD5, &sMD5Context);
 }
 
-void CalculateRSA(char *pMessage, char *_pRSA, bool _bUseKey2)
+void CalculateRSA(char *pMessage, unsigned char *_pRSA, bool _bUseKey2)
 {
   BIGD	n1	= bdNew();
   BIGD	d1	= bdNew();
