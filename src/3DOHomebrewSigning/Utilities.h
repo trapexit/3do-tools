e59e83b959abc55bbd1d1351a379924af9de3195
Antonio SJ Musumeci <trapexit@spawn.link>
2025-01-21 21:50:21 -0600

    Utilities.h

--- src/3DOHomebrewSigning/Utilities.h
+++ src/3DOHomebrewSigning/Utilities.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Calculate MD5 checksum
-void CalculateMD5(BYTE *_pBuffer, int _iDataSize, BYTE *_pMD5);
+void CalculateMD5(char *_pBuffer, int _iDataSize, char *_pMD5);
 
 // Calculate RSA
-void CalculateRSA(char *pMessage, BYTE *_pRSA, bool _bUseKey2 = false);
+void CalculateRSA(char *pMessage, char *_pRSA, bool _bUseKey2 = false);
