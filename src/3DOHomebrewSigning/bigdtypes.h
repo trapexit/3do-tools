f649d7750cf7c058555f29600b92de2602733e5d
Antonio SJ Musumeci <trapexit@spawn.link>
2025-01-21 22:05:01 -0600

    bigdtypes.h

--- src/3DOHomebrewSigning/bigdtypes.h
+++ src/3DOHomebrewSigning/bigdtypes.h
@@ -11,10 +11,10 @@ library or can be obtained from <www.di-mgt.com.au/bigdigits.html>.
 This notice must always be retained in any copy.
 ******************* END OF COPYRIGHT NOTICE***************************/
 /*
-	Last updated:
-	$Date: 2008-05-04 13:05:00 $
-	$Revision: 2.2.0 $
-	$Author: dai $
+  Last updated:
+  $Date: 2008-05-04 13:05:00 $
+  $Revision: 2.2.0 $
+  $Author: dai $
 */
 
 #ifndef BIGDTYPES_H_
@@ -23,43 +23,43 @@ This notice must always be retained in any copy.
 #include <stddef.h>
 
 /*
-The following PP instructions assume that all Linux systems have a C99-conforming 
-<stdint.h>; that other Unix systems have the uint32_t definitions in <sys/types.h>;
-and that MS et al don't have them at all. This version assumes that a long is 32 bits.
-Adjust if necessary to suit your system. 
-You can override by defining HAVE_C99INCLUDES or HAVE_SYS_TYPES.
+  The following PP instructions assume that all Linux systems have a C99-conforming 
+  <stdint.h>; that other Unix systems have the uint32_t definitions in <sys/types.h>;
+  and that MS et al don't have them at all. This version assumes that a long is 32 bits.
+  Adjust if necessary to suit your system. 
+  You can override by defining HAVE_C99INCLUDES or HAVE_SYS_TYPES.
 */
 
 #ifndef EXACT_INTS_DEFINED_
 #define EXACT_INTS_DEFINED_ 1
 #ifndef HAVE_C99INCLUDES
-	#if (__STDC_VERSION >= 199901L) || defined(linux) || defined(__linux__)
-	#define HAVE_C99INCLUDES
-	#endif
+#if (__STDC_VERSION >= 199901L) || defined(linux) || defined(__linux__)
+#define HAVE_C99INCLUDES
+#endif
 #endif
 #ifndef HAVE_SYS_TYPES
-	#if defined(unix) || defined(__unix__)
-	#define HAVE_SYS_TYPES
-	#endif
+#if defined(unix) || defined(__unix__)
+#define HAVE_SYS_TYPES
+#endif
 #endif
 #ifdef HAVE_C99INCLUDES
-	#include <stdint.h>
+#include <stdint.h>
 #elif defined(HAVE_SYS_TYPES)
-	#include <sys/types.h>
+#include <sys/types.h>
 #else 
-	#define uint32_t unsigned long 
-	#define uint16_t unsigned short 
+#define uint32_t unsigned long 
+#define uint16_t unsigned short 
 #endif	/* HAVE_C99INCLUDES */
 #endif	/* EXACT_INTS_DEFINED_ */
 
 /* Macros for format specifiers 
--- change to "u", "x" and "X" if necessary */
+   -- change to "u", "x" and "X" if necessary */
 #ifdef HAVE_C99INCLUDES
-	#include <inttypes.h>
+#include <inttypes.h>
 #else 
-	#define PRIu32 "lu" 
-	#define PRIx32 "lx" 
-	#define PRIX32 "lX" 
+#define PRIu32 "lu" 
+#define PRIx32 "lx" 
+#define PRIX32 "lX" 
 #endif
 /* We define our own */
 #define PRIuBIGD PRIu32
