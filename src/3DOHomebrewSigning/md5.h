a8b6847e41dd3713676d58d9ed8ad6aaeae3d0ad
Antonio SJ Musumeci <trapexit@spawn.link>
2025-01-21 22:30:56 -0600

    md5.h

--- src/3DOHomebrewSigning/md5.h
+++ src/3DOHomebrewSigning/md5.h
@@ -40,8 +40,8 @@ typedef struct {
 } MD5_CTX;
 
 void MD5Init PROTO_LIST ((MD5_CTX *));
-void MD5Update PROTO_LIST ((MD5_CTX *, unsigned char *, unsigned int));
-void MD5Final PROTO_LIST ((unsigned char [16], MD5_CTX *));
+void MD5Update PROTO_LIST ((MD5_CTX *, char *, unsigned int));
+void MD5Final PROTO_LIST ((char [16], MD5_CTX *));
 
 #ifdef __cplusplus
 }
